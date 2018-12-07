6ee57fff43e6e7c99983681e268ef9a2bb0b9619
ag5300cm <bhillenbrand101@gmail.com>
2018-12-07 14:15:35 -0600

    Add files via upload

---
+++ Gems/BlueGem.gd
@@ -0,0 +1,32 @@
+extends Area2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+func _physics_process(delta): 
+	var bodies = get_overlapping_bodies() # sees if anything is touchin it 
+	print(bodies) 
+	for body in bodies :
+		if body.name == "Player" :
+			
+			self.queue_free()  # this will delete the gem once the player touches it. 
+			pass
+			#get_tree().change_scene("World2.tscn") 
+			
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+#func _on_BlueGem_body_entered(body):
+#	print("BlueGem Grabed")
+	
+	
+	
